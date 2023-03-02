d8eb6da319c9ca4724f55626636f929b1df9296d
roschel <joaomroschel@gmail.com>
2023-03-01 22:01:17 -0300

    creating models

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Course, Evaluation
+
+
 # Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'created_at', 'updated_at', 'active')
+
+
+@admin.register(Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ('course', 'name', 'email', 'grade', 'created_at', 'updated_at', 'active')
--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Course(Base):
+    title = models.CharField(max_length=255)
+    url = models.URLField(unique=True)
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+
+    def __str__(self) -> str:
+        return self.title
+    
+class Evaluation(Base):
+    course = models.ForeignKey(Course, related_name='avaliações', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    comment =models.TextField(blank=True, default='')
+    grade = models.DecimalField(max_digits=2, decimal_places=1)
+
+    class Meta:
+        verbose_name='Avaliação'
+        verbose_name_plural='Avaliações'
+        unique_together = ['email', 'course']
+
+    def __str__(self) -> str:
+        return f"{self.name} avaliou o curso {self.course} com nota {self.grade}"
