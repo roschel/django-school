e82aba063a7b24015c86078585e1ad6199375138
roschel <joaomroschel@gmail.com>
2023-03-05 21:21:39 -0300

    post method

--- cursos/views.py
+++ cursos/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Course, Evaluation
 
 from .serializers import CourseSerializer, EvaluationSerializer
@@ -15,6 +16,12 @@ class CourseAPIView(APIView):
         serializer = CourseSerializer(courses, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class EvaluationAPIView(APIView):
     """
@@ -25,3 +32,9 @@ class EvaluationAPIView(APIView):
         evaluations = Evaluation.objects.all()
         serializer = EvaluationSerializer(evaluations, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = EvaluationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
