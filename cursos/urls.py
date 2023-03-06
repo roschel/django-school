d077d3bdbd4c7e95c229a1424a768383b7649bff
roschel <joaomroschel@gmail.com>
2023-03-05 22:57:20 -0300

    sobreescrevendo metodos do generics

--- cursos/urls.py
+++ cursos/urls.py
@@ -5,6 +5,9 @@ from .views import CourseAPIView, EvaluationAPIView, CoursesAPIView, Evaluations
 urlpatterns = [
     path('courses/', CoursesAPIView.as_view(), name='courses'),
     path('courses/<int:pk>', CourseAPIView.as_view(), name='course'),
+    path('courses/<int:course_pk>/evaluations/', EvaluationsAPIView.as_view(), name='course_evaluations'),
+    path('courses/<int:course_pk>/evaluations/<int:evaluation_pk>', EvaluationAPIView.as_view(),
+         name='course_evaluation'),
     path('evaluations/', EvaluationsAPIView.as_view(), name='evaluations'),
-    path('evaluations/<int:pk>', EvaluationAPIView.as_view(), name='evaluation'),
+    path('evaluations/<int:evaluation_pk>', EvaluationAPIView.as_view(), name='evaluation'),
 ]
