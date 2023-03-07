6f3258b841ea69e29189a784272c844a0bfcfc9c
roschel <joaomroschel@gmail.com>
2023-03-06 23:23:06 -0300

    relação entre models no serializer

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -21,6 +21,15 @@ class EvaluationSerializer(serializers.ModelSerializer):
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    # Nested Relationship
+    # evaluations = EvaluationSerializer(many=True, read_only=True)
+
+    # HyperLinked Related Field
+    # evaluations = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='evaluation-detail')
+
+    # Primary Key Related Field
+    evaluations = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Course
         fields = '__all__'
